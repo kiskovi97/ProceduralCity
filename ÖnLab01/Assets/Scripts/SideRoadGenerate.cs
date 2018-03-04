ce00c760b01216210d82d68578e3685c435d00ca
anadutova <d.anasztazia@gmail.com>
2018-03-04 15:47:34 +0100

    SideRoadObjects

---
+++ ÖnLab01/Assets/Scripts/SideRoadGenerate.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SideRoadGenerate : MonoBehaviour {
+    public SideRoadObjects objects;
+	// Use this for initialization
+	void Start () {
+        Invoke("GeneratePadokX", 0.5f);
+        Invoke("GeneratePadokY", 1.0f);
+    }
+	
+	// Update is called once per frame
+	void GeneratePadokX () {
+        GameObject pad = objects.PadX;
+        float z = Random.value * 1.5f - 0.75f;
+        pad.transform.SetParent(this.transform);
+        pad.transform.localScale = transform.localScale * 0.5f;
+        pad.transform.localRotation = new Quaternion(0, 0, 0, 0);
+        pad.transform.Rotate(0, 90, 0);
+        pad.transform.position = new Vector3(0,0,0);
+        pad.transform.localPosition = new Vector3(-0.9f, 0.02f, z);
+
+        
+        
+	}
+    void GeneratePadokY()
+    {
+        GameObject pad = objects.PadX;
+        float z = Random.value * 1.5f - 0.75f;
+        pad.transform.SetParent(this.transform);
+        pad.transform.localScale = transform.localScale * 0.5f;
+        pad.transform.localRotation = new Quaternion(0, 0, 0, 0);
+        pad.transform.Rotate(0, 270, 0);
+        pad.transform.position = new Vector3(0, 0, 0);
+        pad.transform.localPosition = new Vector3(0.9f, 0.02f, z);
+    }
+}
---
+++ ÖnLab01/Assets/Scripts/SideRoadObjects.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SideRoadObjects : MonoBehaviour
+{
+
+    public List<GameObject> padok = new List<GameObject>();
+
+    public GameObject PadX
+    {
+        get
+        {
+            int i = (int)(Random.value * padok.Count);
+            if (i == padok.Count) i--;
+            GameObject ki = Instantiate(padok[i]);
+            return ki;
+        }
+    }
+}
