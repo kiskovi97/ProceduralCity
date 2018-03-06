0236b15ac97c9796104f4d2462d0c8ce796b164c
kiskovi97 <kiskovi97@gmail.com>
2018-03-06 01:02:22 +0100

    MultipleCrossing and growing

--- ÖnLab01/Assets/Scripts/AdvancedScript/RoadNode.cs
+++ ÖnLab01/Assets/Scripts/AdvancedScript/RoadNode.cs
@@ -9,6 +9,7 @@ public class RoadNode : MonoBehaviour {
     public Vector3 elozo_irany = new Vector3(0, 0, 1);
     public Vector3 elozo_pont07 = new Vector3(1, 0, 10);
     public Vector3 elozo_pont08 = new Vector3(-1, 0, 10);
+    public float grow = 1.0f;
     Mesh mesh;
     void Awake()
     {
@@ -21,71 +22,41 @@ public class RoadNode : MonoBehaviour {
         MakeIranyok();
         MakeMeshData();
         CreateMesh();
+        if (grow>0)
+            CreateChilds();
 	}
 
     List<Vector3> tovabb_irany = new List<Vector3>();
     void MakeIranyok()
     {
+        int elagazasok = (int)(Random.value *8);
+        if (elagazasok < 5) elagazasok = 1;
+        else elagazasok -= 4;
+        Debug.Log(elagazasok);
+        for (int i=1; i<elagazasok+1; i++)
+        {
+            Vector3 uj = new Vector3();
+            float Rotation = 3.14f * ( 2 * (-i/(elagazasok+1.0f)));
+            Rotation += -0.2f + Random.value * 0.4f;
+            uj.Set(
+            elozo_irany.x * Mathf.Cos(Rotation) - elozo_irany.z * Mathf.Sin(Rotation), elozo_irany.y * -1,
+            elozo_irany.x * Mathf.Sin(Rotation) + elozo_irany.z * Mathf.Cos(Rotation));
+            tovabb_irany.Add(uj.normalized);
+        }
+
         
-        Vector3 uj = new Vector3();
-        float Rotation = 3.14f * (-120.0f / 180);
-        uj.Set(
-            elozo_irany.x * Mathf.Cos(Rotation) - elozo_irany.z * Mathf.Sin(Rotation), elozo_irany.y * -1,
-            elozo_irany.x * Mathf.Sin(Rotation) + elozo_irany.z * Mathf.Cos(Rotation));
-        uj.Normalize();
-        tovabb_irany.Add(uj);
-
-        Rotation = 3.14f * (-240.0f / 180);
-        Vector3 uj2 = new Vector3();
-        uj2.Set(
-            elozo_irany.x * Mathf.Cos(Rotation) - elozo_irany.z * Mathf.Sin(Rotation), elozo_irany.y * -1,
-            elozo_irany.x * Mathf.Sin(Rotation) + elozo_irany.z * Mathf.Cos(Rotation));
-        uj2.Normalize();
-        tovabb_irany.Add(uj2);
-
     }
     // Adatok az objektum letrehozasahoz
     List<Vector3> vertexes = new List<Vector3>();
     
-    List<Vector3> normals = new List<Vector3>();
     List<int> triangles = new List<int>();
     // Update is called once per frame
     void MakeMeshData () {
-        Vector3 normal = new Vector3(0, 1, 0);
-        Vector3 pos = transform.position;
-        vertexes.Add(pos);
-        normals.Add(normal);
-        Vector3 iranyJobbra = new Vector3();
-        Vector3 iranyBalra = new Vector3();
-        foreach (Vector3 irany in tovabb_irany)
-        {
-            iranyJobbra.Set(irany.z, irany.y, irany.x * -1);
-            iranyBalra.Set(irany.z * -1, irany.y, irany.x);
-            vertexes.Add(pos + irany*radius + iranyBalra*radius2);
-            vertexes.Add(pos + irany*radius + iranyJobbra * radius2);
-            
-            normals.Add(normal);
-            normals.Add(normal);
-        }
-        iranyJobbra.Set(elozo_irany.z, elozo_irany.y, elozo_irany.x * -1);
-        iranyBalra.Set(elozo_irany.z * -1, elozo_irany.y, elozo_irany.x);
-        vertexes.Add(pos + elozo_irany*radius + iranyBalra * radius2);
-        vertexes.Add(pos + elozo_irany*radius + iranyJobbra * radius2);
-        normals.Add(normal);
-        normals.Add(normal);
-        for (int i=1; i<vertexes.Count-1; i += 1)
-        {
-            triangles.Add(0);
-            triangles.Add(i);
-            triangles.Add(i + 1);
-        }
-        triangles.Add(0);
-        triangles.Add(vertexes.Count - 1);
-        triangles.Add(1);
-
-        // elozovel osszekotes
-        normals.Add(normal);
-        normals.Add(normal);
+        MakeCenter();
+        MakeElozoUt();
+    }
+    void MakeElozoUt()
+    {
         vertexes.Add(elozo_pont07);
         vertexes.Add(elozo_pont08);
         triangles.Add(vertexes.Count - 1);
@@ -95,16 +66,62 @@ public class RoadNode : MonoBehaviour {
         triangles.Add(vertexes.Count - 2);
         triangles.Add(vertexes.Count - 3);
         triangles.Add(vertexes.Count - 4);
+    }
+    void MakeCenter()
+    {
+        
+        vertexes.Add(new Vector3(0,0,0));
+        Vector3 iranyJobbra = new Vector3();
+        Vector3 iranyBalra = new Vector3();
+        foreach (Vector3 irany in tovabb_irany)
+        {
+            iranyJobbra.Set(irany.z, irany.y, irany.x * -1);
+            iranyBalra.Set(irany.z * -1, irany.y, irany.x);
+            vertexes.Add(irany * radius + iranyBalra * radius2);
+            vertexes.Add(irany * radius + iranyJobbra * radius2);
 
-
+        }
+        iranyJobbra.Set(elozo_irany.z, elozo_irany.y, elozo_irany.x * -1);
+        iranyBalra.Set(elozo_irany.z * -1, elozo_irany.y, elozo_irany.x);
+        vertexes.Add(elozo_irany * radius + iranyBalra * radius2);
+        vertexes.Add(elozo_irany * radius + iranyJobbra * radius2);
+        for (int i = 1; i < vertexes.Count - 1; i += 1)
+        {
+            triangles.Add(0);
+            triangles.Add(i);
+            triangles.Add(i + 1);
+        }
+        triangles.Add(0);
+        triangles.Add(vertexes.Count - 1);
+        triangles.Add(1);
     }
     void CreateMesh()
     {
         mesh.Clear();
         mesh.vertices = vertexes.ToArray();
         mesh.triangles = triangles.ToArray();
+        mesh.RecalculateNormals();
+    }
+    void CreateChilds()
+    {
 
-        mesh.SetNormals(normals);
+        foreach (Vector3 irany in tovabb_irany)
+        {
+            GameObject uj = Instantiate(this.gameObject);
+            uj.transform.position += irany * 30;
+            RoadNode n = uj.GetComponent<RoadNode>();
+            n.grow = grow - 0.1f;
+            Vector3 iranyJobbra = new Vector3();
+            Vector3 iranyBalra = new Vector3();
+            iranyJobbra.Set(irany.z, irany.y, irany.x * -1);
+            iranyBalra.Set(irany.z * -1, irany.y, irany.x);
+
+            n.elozo_irany = (transform.position - uj.transform.position).normalized;
+            n.elozo_pont07 = (transform.position + irany.normalized * radius + iranyBalra * radius2) - uj.transform.position;
+            n.elozo_pont08 = (transform.position + irany.normalized * radius + iranyJobbra * radius2) - uj.transform.position;
+        }
+
+        
     }
 
 }
