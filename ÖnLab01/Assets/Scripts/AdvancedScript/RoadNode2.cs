03409bf8fa83b601dbef0866c1f85b3fd572cf77
anadutova <d.anasztazia@gmail.com>
2018-03-09 16:28:28 +0100

    SimpleComplexRoads 2.0
    With Debug.DrawLine()
    Without Generating Mesh
    Just one Road

---
+++ ÖnLab01/Assets/Scripts/AdvancedScript/RoadNode2.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadNode2 {
+    RoadNode2 elozo = null;
+    List<RoadNode2> szomszedok;
+    public Vector3 position;
+    
+    // Use this for initialization
+   
+    public RoadNode2() {
+        szomszedok = new List<RoadNode2>();
+        position = new Vector3(0, 0, 0);
+    }
+    public void SetPosition(Vector3 pos)
+    {
+        
+        position = pos;
+    }
+
+    List<Vector3> tovabb_irany = new List<Vector3>();
+    void MakeIranyok()
+    {
+        Vector3 elozo_irany = new Vector3(0, 0, 1);
+        if (elozo != null) elozo_irany = position - elozo.position;
+
+        int elagazasok = (int)(Random.value * 8);
+        if (elagazasok < 5) elagazasok = 1;
+        else elagazasok -= 4;
+        Debug.Log(elagazasok);
+        for (int i = 1; i < elagazasok + 1; i++)
+        {
+            Vector3 uj = new Vector3();
+            float Rotation = 3.14f * (2 * (-i / (elagazasok + 1.0f)));
+            Rotation += -0.2f + Random.value * 0.4f;
+            uj.Set(
+            elozo_irany.x * Mathf.Cos(Rotation) - elozo_irany.z * Mathf.Sin(Rotation), elozo_irany.y * -1,
+            elozo_irany.x * Mathf.Sin(Rotation) + elozo_irany.z * Mathf.Cos(Rotation));
+            tovabb_irany.Add(uj.normalized);
+        }
+
+
+    }
+
+    public void SetElozo(RoadNode2 setElozo)
+    {
+        elozo = setElozo;
+        if (szomszedok.Count == 0)
+            szomszedok.Add(elozo);
+        else
+            szomszedok[0] = setElozo;
+        Debug.DrawLine(position, elozo.position, Color.red, 100, false);
+        Debug.Log("Itt is jartunk" + position.ToString() + elozo.position.ToString());
+    }
+    public List<RoadNode2> GenerateRoads()
+    {
+        List<RoadNode2> ki = new List<RoadNode2>();
+        
+        RoadNode2 ad = new RoadNode2();
+        Vector3 irany = new Vector3(0, 0, 1);
+        if (elozo!=null) irany = position - elozo.position;
+        ad.position = position + irany + new Vector3(Random.value*0.9f-0.45f, 0, 0);
+        ad.SetElozo(this);
+        ki.Add(ad);
+        
+        return ki;
+    }
+    
+	
+}
---
+++ ÖnLab01/Assets/Scripts/AdvancedScript/RoadNodeContainer.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadNodeContainer : MonoBehaviour {
+  
+    public GameObject visual;
+    public List<RoadNode2> roads = new List<RoadNode2>();
+    int rootObjIndex = 0;
+    public float xMin = -20;
+    public float zMin = -20;
+    public float xMax = 20;
+    public float zMax = 20;
+    // Use this for initialization
+    void Start() {
+        roads.Clear();
+        roads.Add(new RoadNode2());
+        Invoke("Visualization01", 0.01f);
+        Invoke("Generating",0.1f);
+    }
+    void Visualization01()
+    {
+        foreach (RoadNode2 road in roads)
+        {
+            GameObject ki = Instantiate(visual);
+            ki.transform.position = road.position;
+        }
+    }
+    bool  PalyanBelulVane(RoadNode2 r)
+    {
+        if (r.position.x < xMin || r.position.x > xMax)
+        {
+            Debug.Log("Kint Vagy");
+            return false;
+        }
+        if (r.position.z < zMin || r.position.z > zMax)
+        {
+            Debug.Log("Kint Vagy");
+            return false;
+        }
+        return true;
+    }
+    void Generating(){
+        if (rootObjIndex < roads.Count)
+        {
+            RoadNode2 root = roads[rootObjIndex];
+            if (PalyanBelulVane(root))
+            {
+                List<RoadNode2> newRoads = root.GenerateRoads();
+                foreach (RoadNode2 road in newRoads)
+                {
+                    roads.Add(road);
+                    GameObject ki = Instantiate(visual);
+                    ki.transform.position = road.position;
+                }
+            }
+            rootObjIndex++;
+            Generating();
+        }
+        else return;
+    }
+	
+}
