340e833145ca17df4dc1aef08f90d59b05f85b84
kiskovi97 <kiskovi97@gmail.com>
2018-04-11 00:26:06 +0200

    Refactoring and better methods
    the prependicular calculating errors corrected

--- ÖnLab01/Assets/Scripts/AdvancedScript/BlockObjectScript.cs
+++ ÖnLab01/Assets/Scripts/AdvancedScript/BlockObjectScript.cs
@@ -80,6 +80,7 @@ public class BlockObjectScript : MonoBehaviour {
         Vector3 meroleges = elsoMeroleges(0);
         for (int i=1; i < controlPoints.Count; i++)
         {
+            //Vector3 merolegese = Kereszt(i);
             meroleges = NextLepes(i, meroleges);
         }
         meroleges = NextLepes(0, meroleges);
@@ -129,38 +130,15 @@ public class BlockObjectScript : MonoBehaviour {
 
         Vector3 actual_point = controlPoints[index];
         Vector3 next_point;
-        if (index + 1 < controlPoints.Count)
-            next_point = controlPoints[index + 1];
-        else
-            next_point = controlPoints[0];
+        if (index + 1 < controlPoints.Count) next_point = controlPoints[index + 1];
+        else next_point = controlPoints[0];
 
         Vector3 elozo_point;
-        if (index - 1 >= 0)
-            elozo_point = controlPoints[index - 1];
-        else
-            elozo_point = controlPoints[controlPoints.Count - 1];
+        if (index - 1 >= 0) elozo_point = controlPoints[index - 1];
+        else elozo_point = controlPoints[controlPoints.Count - 1];
 
-        Vector3 irany_elozo = (elozo_point - actual_point).normalized;
-        Vector3 irany_kovetkezo = (next_point - actual_point).normalized;
-
-        Vector3 actual_meroleges;
-        Vector3 egyik = irany_elozo + irany_kovetkezo;
-        Vector3 masik = (irany_elozo + irany_kovetkezo)*(-1);
-        Vector3 kozeppont_irany = kozeppont - actual_point;
-        if (egyik.sqrMagnitude < 0.01f)
-        {
-            Vector3 bal = new Vector3(irany_elozo.z, 0, -irany_elozo.x);
-            Vector3 jobb = new Vector3(-irany_elozo.z, 0, irany_elozo.x);
-            bal.Normalize();
-            jobb.Normalize();
-            actual_meroleges =  Vector3.Angle(kozeppont_irany, bal) > Vector3.Angle(kozeppont_irany, jobb)? jobb.normalized : bal.normalized;
-        }
-        else
-        {
-            actual_meroleges = Vector3.Angle(kozeppont_irany, egyik) > Vector3.Angle(kozeppont_irany, masik) ? masik.normalized : egyik.normalized;
-        }
-
-        Vector3 felezo_irany = (elozo_meroleges + actual_meroleges).normalized;
+        Vector3 actual_meroleges = Kereszt(next_point,elozo_point,actual_point);
+        Vector3 felezo_irany = Meroleges(elozo_point,actual_point);
 
         float hosz = (elozo_point - actual_point).magnitude;
         float arany = (hosz - minHouse) / hosz;
@@ -169,7 +147,6 @@ public class BlockObjectScript : MonoBehaviour {
         {
             arany = 0.5f;
             felezo_point2 = (actual_point * (1.0f - arany) + elozo_point * arany);
-
             Vector3 FB = felezo_point2 + felezo_irany.normalized * b;
             Vector3 AB = actual_point + actual_meroleges.normalized * b;
             MakeBox(felezo_point2, actual_point, AB, FB);
@@ -189,7 +166,7 @@ public class BlockObjectScript : MonoBehaviour {
 
     Vector3 NextLepes(Vector3 actual_point, Vector3 next_point, Vector3 elozo_point, Vector3 elozo_meroleges)
     {
-        elozo_meroleges.Normalize();
+
         float a = Random.value * (HouseDeepmax - HouseDeepmin) + HouseDeepmin;
         float b = Random.value * (HouseDeepmax - HouseDeepmin) + HouseDeepmin;
 
@@ -197,24 +174,9 @@ public class BlockObjectScript : MonoBehaviour {
         Vector3 irany_kovetkezo = (next_point - actual_point).normalized;
 
 
-        Vector3 actual_meroleges;
-        Vector3 egyik = irany_elozo + irany_kovetkezo;
-        Vector3 masik = (irany_elozo + irany_kovetkezo) * (-1);
-        Vector3 kozeppont_irany = kozeppont - actual_point;
-        if (egyik.sqrMagnitude < 0.01f)
-        {
-            Vector3 bal = new Vector3(irany_elozo.z, 0, -irany_elozo.x);
-            Vector3 jobb = new Vector3(-irany_elozo.z, 0, irany_elozo.x);
-            bal.Normalize();
-            jobb.Normalize();
-            actual_meroleges = Vector3.Angle(kozeppont_irany, bal) > Vector3.Angle(kozeppont_irany, jobb) ? jobb.normalized : bal.normalized;
-        }
-        else
-        {
-            actual_meroleges = Vector3.Angle(kozeppont_irany, egyik) > Vector3.Angle(kozeppont_irany, masik) ? masik.normalized : egyik.normalized;
-        }
+        Vector3 actual_meroleges = Meroleges(elozo_point, actual_point);
 
-        Vector3 felezo_irany = (elozo_meroleges + actual_meroleges).normalized;
+        Vector3 felezo_irany = Meroleges(elozo_point, actual_point);
 
         float hosz = (elozo_point - actual_point).magnitude;
         float arany = (hosz - minHouse) / hosz;
@@ -234,14 +196,95 @@ public class BlockObjectScript : MonoBehaviour {
             felezo_point2 = NextLepes(actual_point, next_point, tmp, felezo_irany);
         }
         Vector3 felezo_point = (actual_point * (1.0f - arany) + elozo_point * arany);
-
         Vector3 EA = elozo_point + elozo_meroleges.normalized * a;
         Vector3 FA = felezo_point + felezo_irany.normalized * a;
         MakeBox(elozo_point, felezo_point, FA, EA);
         return felezo_point2;
     }
 
+    
+    Vector3 Kereszt(Vector3 next_point, Vector3 elozo_point, Vector3 actual_point)
+    {
+        Vector3 next_irany = (next_point - actual_point).normalized;
+        Vector3 elozo_irany = (elozo_point - actual_point).normalized;
+        Vector3 kereszt = (next_irany + elozo_irany).normalized;
+        if (befele(actual_point + kereszt * 0.01f, kereszt))
+            Debug.DrawLine(actual_point, actual_point + kereszt * 0.5f, Color.green, 100);
+        else
+        {
+            Debug.DrawLine(actual_point, actual_point + kereszt * 0.5f, Color.black, 100);
+            kereszt *= -1;
+            Debug.DrawLine(actual_point, actual_point + kereszt * 0.5f, Color.green, 100);
+        }
 
+        Debug.DrawLine(actual_point, actual_point + next_irany * 0.2f, Color.red, 100);
+        return kereszt;
+    }
+
+    Vector3 Meroleges(Vector3 actual_point, Vector3 next_point)
+    {
+        Vector3 next_irany = (next_point - actual_point).normalized;
+        Quaternion rotation = Quaternion.Euler(0, 90, 0);
+        Vector3 meroleges = (rotation * next_irany).normalized;
+
+        Debug.DrawLine(actual_point, actual_point + meroleges * 0.5f, Color.red, 100);
+        Debug.DrawLine(actual_point, actual_point + next_irany * 0.2f, Color.red, 100);
+        return meroleges;
+    }
+
+
+    bool befele(Vector3 from, Vector3 dir)
+    {
+        int db = 0;
+        for (int i=0; i<controlPoints.Count-1; i++)
+        {
+            if(Metszie(controlPoints[i], controlPoints[i + 1], from, dir)) db++;
+        }
+        if (Metszie(controlPoints[0], controlPoints[controlPoints.Count-1], from, dir)) db++;
+        return db % 2 == 1;
+    }
+
+    bool Metszie(Vector3 A, Vector3 B, Vector3 P, Vector3 direction)
+    {
+        // 2Dbe konvertalas XZ sikra levetitve
+        direction.Normalize();
+        float ax = A.x;
+        float ay = A.z;
+        float bx = B.x;
+        float by = B.z;
+        float px = P.x;
+        float py = P.z;
+        float vx = direction.x;
+        float vy = direction.z;
+        if ((vx * (ay - by) - vy * (ax - bx)) == 0)
+        {
+            Debug.Log("ZeroDivide");
+            return false;
+        }
+        float q = (vx * (py - by) - vy * (px - bx))/
+                    (vx * (ay - by) - vy * (ax - bx));
+        float t = 0;
+        if (vx == 0)
+        {
+            if (vy == 0)
+            {
+                Debug.Log("ZeroDivide");
+                return false;
+            }
+            else
+            {
+                t = (q * ay + (1 - q) * by - py) / vy;
+            }
+        } else
+        {
+            t = (q * ax + (1 - q) * bx - px) / vx;
+        }
+
+        if (t < 0) return false;
+        if (q < 0) return false;
+        if (q > 1) return false;
+        return true;
+    }
     List<Color> colors=new List<Color>();
     public void CreateMesh()
     {
