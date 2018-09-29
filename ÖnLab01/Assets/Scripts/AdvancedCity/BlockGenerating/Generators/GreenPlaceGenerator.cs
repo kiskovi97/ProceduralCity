de827431027e26bca9157a8b13d68df162b60e6c
Kovács Gergő <kiskovi97@gmail.com>
2018-09-29 15:53:34 +0200

    Other Options

--- ÖnLab01/Assets/Scripts/AdvancedCity/BlockGenerating/Generators/GreenPlaceGenerator.cs
+++ ÖnLab01/Assets/Scripts/AdvancedCity/BlockGenerating/Generators/GreenPlaceGenerator.cs
@@ -5,6 +5,11 @@ class GreenPlaceGenerator : GeneratorImpl
 {
     public GreenPlaceGenerator(List<Vector3> kontrolpoints)
     {
-        elements.Add(new PlaceMesh(kontrolpoints));
+        List<Vector3> points = new List<Vector3>();
+        for (int i=0; i< kontrolpoints.Count; i++)
+        {
+            points.Add(kontrolpoints[i] + new Vector3(0,-0.1f,0));
+        }
+        elements.Add(new PlaceMesh(points));
     }
 }
